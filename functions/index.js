0eedd1d0ab5e681de032820261890af55a742d20
Tarek <bayridge4th@gmail.com>
2019-08-17 08:51:00 -0400

    user reg and validation

--- functions/index.js
+++ functions/index.js
@@ -56,10 +56,16 @@ app.post('/post',(req,res)=>{
     })
 })
 
+const isEmail = email => {
+  const reqEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  email.match(reqEx) ? true : false ;
+}
 
-
+const isEmpty = string =>{
+  if(string.trim() === '') return true;
+  else return false;
+}
 //Sign up route
-
 app.post('/signup',(req,res)=>{
   const newUser = {
     email:req.body.email,
@@ -68,15 +74,53 @@ app.post('/signup',(req,res)=>{
     handle:req.body.handle
   }
 
+  let errors = {};
+  if(isEmpty(newUser.email)) errors.email = 'Email must not be empty'
+  else if(isEmail(newUser.email)) errors.email = 'Must be a valid email address'
+  
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+  if((Object.keys(errors)).length > 0) return res.status(400).json(errors)
   //TODO validate data
-  firebase.auth().createUserWithEmailAndPassword(req.body.email,req.body.password)
-    .then(data=>{
-      //status 201 is resource created
-      return res.status(201).json({message:`user ${data.user.uid} signed up successfully`})
+  let token,userId;
+  db.doc(`users/${newUser.handle}`).get()
+    .then(doc =>{
+      if(doc.exists){
+        //bad request
+        return res.status(400).json({handle: 'this handle is already taken'})
+      } else{
+        return firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password)
+      }
+    })
+    .then(data =>{
+      userId = data.user.uid
+      return data.user.getIdToken()
+    })
+    .then(idToken =>{
+      token = idToken;
+      const userCredentials = {
+        handle:newUser.handle,
+        email:newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+        
+      }
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(()=>{
+      return res.status(201).json({token});
     })
     .catch(err=>{
       console.error(err);
-      return res.status(500).json({err:err.code}) //server error
+      if(err.code === 'auth/email-already-in-use'){
+        return res.status(400).json({email:'Email is already in use'})
+      }
+      else return res.status(500).json({error:err.code})
+      
     })
+  
 })
+
 exports.api = functions.https.onRequest(app);
