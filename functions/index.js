4c7475f510b93fef4c01591a6e3bb7a1b84e731c
Tarek <bayridge4th@gmail.com>
2019-08-17 16:01:09 -0400

    middleware authorization

--- functions/index.js
+++ functions/index.js
@@ -39,10 +39,40 @@ app.get('/posts',(req,res)=>{
     .catch(err=> console.log(err));
 })
 
-app.post('/post',(req,res)=>{
+const FBAuth = (req,res,next) =>{
+  let idToken;
+  const {authorization} = req.headers
+
+  if(authorization && authorization.startsWith('Bearer ')){
+    idToken = authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found')
+    return res.status(403).json({error: 'unauthorized'})
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db.collection('users')
+        .where('userId', '==',req.user.uid)
+        .limit(1)
+        .get()
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      next();
+    })
+    .catch(err=>{
+      console.error('Error while verifying token',err);
+      return res.status(403).json(err);
+    })
+}
+
+app.post('/post',FBAuth,(req,res)=>{
   const newPost = {
     body:req.body.body,
-    userHandle:req.body.userHandle,
+    userHandle:req.user.handle,
     createdAt: new Date().toISOString()
   }
 
@@ -58,7 +88,8 @@ app.post('/post',(req,res)=>{
 
 const isEmail = email => {
   const reqEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  email.match(reqEx) ? true : false ;
+  if(email.match(reqEx)) return true
+  else return false
 }
 
 const isEmpty = string =>{
@@ -67,31 +98,32 @@ const isEmpty = string =>{
 }
 //Sign up route
 app.post('/signup',(req,res)=>{
-  const newUser = {
-    email:req.body.email,
-    password:req.body.password,
-    confirmPassword:req.body.confirmPassword,
-    handle:req.body.handle
-  }
+  const {email,password,confirmPassword,handle} = req.body
+  // const newUser = {
+  //   email:req.body.email,
+  //   password:req.body.password,
+  //   confirmPassword:req.body.confirmPassword,
+  //   handle:req.body.handle
+  // }
 
   let errors = {};
-  if(isEmpty(newUser.email)) errors.email = 'Email must not be empty'
-  else if(isEmail(newUser.email)) errors.email = 'Must be a valid email address'
+  if(isEmpty(email)) errors.email = 'Email must not be empty'
+  else if(!isEmail(email)) errors.email = 'Must be a valid email address'
   
-  if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
-  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
-  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+  if(isEmpty(password)) errors.password = 'Must not be empty'
+  if(password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+  if(isEmpty(handle)) errors.handle = 'Must not be empty'
 
   if((Object.keys(errors)).length > 0) return res.status(400).json(errors)
   //TODO validate data
   let token,userId;
-  db.doc(`users/${newUser.handle}`).get()
+  db.doc(`users/${handle}`).get()
     .then(doc =>{
       if(doc.exists){
         //bad request
         return res.status(400).json({handle: 'this handle is already taken'})
       } else{
-        return firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password)
+        return firebase.auth().createUserWithEmailAndPassword(email,password)
       }
     })
     .then(data =>{
@@ -101,15 +133,16 @@ app.post('/signup',(req,res)=>{
     .then(idToken =>{
       token = idToken;
       const userCredentials = {
-        handle:newUser.handle,
-        email:newUser.email,
+        handle:handle,
+        email:email,
         createdAt: new Date().toISOString(),
         userId: userId
         
       }
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+      return db.doc(`/users/${handle}`).set(userCredentials);
     })
     .then(()=>{
+      //new resource created
       return res.status(201).json({token});
     })
     .catch(err=>{
@@ -121,6 +154,35 @@ app.post('/signup',(req,res)=>{
       
     })
   
+})
+
+app.post('/login',(req,res)=>{
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  let errors = {}
+  if(isEmpty(user.email)) errors.email = 'Must not be empty';
+  if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+  firebase.auth().signInWithEmailAndPassword(user.email,user.password)
+    .then(data =>{
+      return data.user.getIdToken();
+    })
+    .then(token =>{
+      return res.json(token)
+    })
+    .catch(err=>{
+      console.error(err)
+      if(err.code === 'auth/wrong-password'){
+        //403 is unauthorized error
+        return res.status(403).json({general: 'Wrong credentials, please try again'});
+      }
+      else return res.status(500).json({error:err.code});
+    })
 })
 
 exports.api = functions.https.onRequest(app);
