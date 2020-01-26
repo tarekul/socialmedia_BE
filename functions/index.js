bc1e4dee420a8fd5c882d9bb40a17858dc50e35e
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-01-26 15:49:17 -0500

    added cors for cross browser request

--- functions/index.js
+++ functions/index.js
@@ -1,123 +1,163 @@
-const functions = require('firebase-functions');
-const express = require('express');
+const functions = require("firebase-functions");
+const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
-const {getAllPosts,postOnePost,getPost,
-  commentOnPost,likePost,unlikePost,deletePost} = require('./handlers/posts')
-const {signUpUser,login,uploadImage,addUserDetails,
-  getAuthenticatedUser,getUserDetails,markNotificationsRead} = require('./handlers/user')
-const {middleWare} = require('./utils/middleware')
-const {db} = require('./utils/admin')
+const {
+  getAllPosts,
+  postOnePost,
+  getPost,
+  commentOnPost,
+  likePost,
+  unlikePost,
+  deletePost
+} = require("./handlers/posts");
+const {
+  signUpUser,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead
+} = require("./handlers/user");
+const { middleWare } = require("./utils/middleware");
+const { db } = require("./utils/admin");
 
 //post routes
-app.get('/posts',getAllPosts)
-app.post('/post',middleWare,postOnePost)
-app.get('/post/:postId',getPost)
-app.delete('/post/:postId',middleWare,deletePost)
-app.get('/post/:postId/like',middleWare,likePost)
-app.get('/post/:postId/unlike',middleWare,unlikePost)
-app.post('/post/:postId/comment',middleWare,commentOnPost)
+app.get("/posts", getAllPosts);
+app.post("/post", middleWare, postOnePost);
+app.get("/post/:postId", getPost);
+app.delete("/post/:postId", middleWare, deletePost);
+app.get("/post/:postId/like", middleWare, likePost);
+app.get("/post/:postId/unlike", middleWare, unlikePost);
+app.post("/post/:postId/comment", middleWare, commentOnPost);
 
 //users route
-app.post('/signup',signUpUser)
-app.post('/login',login)
-app.post('/user/image',middleWare,uploadImage)
-app.post('/user',middleWare,addUserDetails)
-app.get('/user',middleWare,getAuthenticatedUser)
-app.get('/user/:handle',getUserDetails)
-app.post('/notifications',middleWare,markNotificationsRead)
-
+app.post("/signup", signUpUser);
+app.post("/login", login);
+app.post("/user/image", middleWare, uploadImage);
+app.post("/user", middleWare, addUserDetails);
+app.get("/user", middleWare, getAuthenticatedUser);
+app.get("/user/:handle", getUserDetails);
+app.post("/notifications", middleWare, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
 
-exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
-  .onCreate((snapshot) => {
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate(snapshot => {
     return db
       .doc(`/posts/${snapshot.data().postId}`)
       .get()
-      .then((doc) => {
-        if(doc.exists && snapshot.data().userHandle !== doc.data().userHandle) {
+      .then(doc => {
+        if (
+          doc.exists &&
+          snapshot.data().userHandle !== doc.data().userHandle
+        ) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
             sender: snapshot.data().userHandle,
-            type: 'like',
+            type: "like",
             read: false,
             postId: doc.id
           });
         }
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   });
 
-exports.deleteNotificationOnUnlike = functions.firestore.document('likes/{id}')
+exports.deleteNotificationOnUnlike = functions.firestore
+  .document("likes/{id}")
   .onDelete(snapshot => {
-    return db.doc(`/notifications/${snapshot.id}`).delete()
-      .catch(err=>{
-        console.error(err)
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch(err => {
+        console.error(err);
         return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document("comments/{id}")
+  .onCreate(snapshot => {
+    return db
+      .doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then(doc => {
+        if (
+          doc.exists &&
+          snapshot.data().userHandle !== doc.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            postId: doc.id
+          });
+        }
       })
-  })
+      .catch(err => console.error(err));
+  });
 
-exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
-  .onCreate(snapshot =>{
-    return db.doc(`/posts/${snapshot.data().postId}`).get()
-    .then(doc=>{
-      if(doc.exists && snapshot.data().userHandle !== doc.data().userHandle){
-        return db.doc(`/notifications/${snapshot.id}`).set({
-          createdAt: new Date().toISOString(),
-          recipient:doc.data().userHandle,
-          sender: snapshot.data().userHandle,
-          type: 'comment',
-          read:false,
-          postId: doc.id
-        })
-      }
-    })
-    .catch(err=> console.error(err))
-  })
-
-exports.onUserImageChange = functions.firestore.document('/users/{userId}')
-  .onUpdate(change =>{
-    console.log(change.before.data())
-    console.log(change.after.data())
-    if(change.before.data().imageUrl !== change.after.data().imageUrl){
+exports.onUserImageChange = functions.firestore
+  .document("/users/{userId}")
+  .onUpdate(change => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
       const batch = db.batch();
-      return db.collection('posts').where('userHandle','==',change.before.data().handle).get()
+      return db
+        .collection("posts")
+        .where("userHandle", "==", change.before.data().handle)
+        .get()
         .then(data => {
-          data.forEach(doc=>{
+          data.forEach(doc => {
             const post = db.doc(`/posts/${doc.id}`);
-            batch.update(post,{userImage:change.after.data().imageUrl});
-          })
-          return batch.commit()
-        })
-    }
-    else return true;
-    
-  })
+            batch.update(post, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
 
-  exports.onPostDelete = functions.firestore.document('/posts/{postId}')
+exports.onPostDelete = functions.firestore
+  .document("/posts/{postId}")
   .onDelete((snapshot, context) => {
     const postId = context.params.postId;
     const batch = db.batch();
-    return db.collection('comments').where('postId', '==', postId).get()
-      .then((data) => {
-        data.forEach((doc) => {
+    return db
+      .collection("comments")
+      .where("postId", "==", postId)
+      .get()
+      .then(data => {
+        data.forEach(doc => {
           batch.delete(db.doc(`/comments/${doc.id}`));
         });
-        return db.collection('likes').where('postId', '==', postId).get();
+        return db
+          .collection("likes")
+          .where("postId", "==", postId)
+          .get();
       })
-      .then((data) => {
-        data.forEach((doc) => {
+      .then(data => {
+        data.forEach(doc => {
           batch.delete(db.doc(`/likes/${doc.id}`));
         });
-        return db.collection('notifications').where('postId', '==', postId).get();
+        return db
+          .collection("notifications")
+          .where("postId", "==", postId)
+          .get();
       })
-      .then((data) => {
-        data.forEach((doc) => {
+      .then(data => {
+        data.forEach(doc => {
           batch.delete(db.doc(`/notifications/${doc.id}`));
         });
         return batch.commit();
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   });
