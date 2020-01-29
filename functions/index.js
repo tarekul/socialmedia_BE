58af34ce824495188e0ee333e2a6ea818f228114
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-01-29 16:32:41 -0500

    fixing cors issue

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,15 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const {
   getAllPosts,
   postOnePost,
